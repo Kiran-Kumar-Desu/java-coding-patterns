fd026b1f637558dc5109700400efb8c1f93c77ed
Kiran-Kumar-Desu <kiran0496@gmail.com>
2025-03-22 06:42:15 +0530

    print matrix in spiral pattern

--- spiral_patterns/MxNmattrix.java
+++ spiral_patterns/MxNmattrix.java
@@ -15,38 +15,51 @@ public class MxNmattrix {
 		int[][] twoDimenMatrix = { { 1, 2, 3, 4, 5, 6 }, { 20, 21, 22, 23, 24, 7 }, { 19, 32, 33, 34, 25, 8 },
 				{ 18, 31, 36, 35, 26, 9 }, { 17, 30, 29, 28, 27, 10 }, { 16, 15, 14, 13, 12, 11 } };
 
-		int top = 0, left = 0, right = 0, bottom = 0;
-		right = bottom = twoDimenMatrix.length - 1;
-		
-		
+//		int[][] twoDimenMatrix = { { 1, 2, 3, 4, 5, 6 }, { 14, 15, 16, 17, 18, 7 }, { 13, 12, 11, 10, 9, 8 } };
 
-		// left -> right (Top row)
-		for (int i = left; i <= right; i++) {
-			System.out.print(twoDimenMatrix[left][i]+" ");
+//		int[][] twoDimenMatrix = { { 1, 2, 3, 4, 5, 6 }};
+
+		int left = 0;
+		int right = twoDimenMatrix[0].length - 1;
+		int top = 0;
+		int bottom = twoDimenMatrix.length - 1;
+
+		int counter = 0;
+
+		while (left <= right && top <= bottom) {
+
+			System.out.println("inner loop time: " + ++counter);
+
+			// left -> right (Top row)
+			for (int i = left; i <= right; i++) {
+				System.out.print(twoDimenMatrix[left][i] + " ");
+			}
+
+			// right -> bottom (right side 1st column)
+			top++;
+			for (int i = top; i <= bottom; i++) {
+				System.out.print(twoDimenMatrix[i][right] + " ");
+			}
+
+			// right -> left (bottom side 1st row)
+			if (top < bottom) {
+				right--;
+				for (int i = right; i >= left; i--) {
+					System.out.print(twoDimenMatrix[bottom][i] + " ");
+				}
+			}
+
+			// bottom -> top (right side 1st column)
+			bottom--;
+			for (int i = bottom; i >= top; i--) {
+				System.out.print(twoDimenMatrix[i][left] + " ");
+			}
+
+			// moving one step towards inner spiral
+			left++;
+
+			System.out.println();
 		}
-		
-		//right -> bottom (right side 1st column)
-		top++;
-		for(int i = top; i <= right; i++) {
-			System.out.print(twoDimenMatrix[i][right]+" ");
-		}
-		
-		System.out.println();
-		
-		//right -> left (bottom side 1st row)
-		right--;
-		for(int i = right; i >= left; i--) {
-			System.out.print(twoDimenMatrix[bottom][i]+" ");
-		}
-		
-		
-		//bottom -> top (right side 1st column)
-		bottom--;
-		for(int i = bottom; i >= top; i--) {
-			System.out.print(twoDimenMatrix[i][left]+" ");
-		}
-		
-		
 
 		/*
 		 * for (int i = 0; i < twoDimenMatrix.length; i++) { for (int j = 0; j <
