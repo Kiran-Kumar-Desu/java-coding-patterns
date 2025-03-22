51dfffc45e9b1088686c1a6a573566460ac3b62f
Kiran-Kumar-Desu <kiran0496@gmail.com>
2025-03-22 12:13:51 +0530

    updated the logic to support different dimensional matrixes

--- spiral_patterns/MxNmattrix.java
+++ spiral_patterns/MxNmattrix.java
@@ -6,7 +6,10 @@ package spiral_patterns;
 	18	31	36	35	26	9
 	17	30	29	28	27	10
 	16	15	14	13	12	11
+	
+	O/P: 1 2 3 4 5 6 7 8 9 10 11 12 ....... 36
 */
+//This logic supports any dimensional array like single row, single column, square and rectangular Matrixes
 
 public class MxNmattrix {
 
@@ -18,6 +21,10 @@ public class MxNmattrix {
 //		int[][] twoDimenMatrix = { { 1, 2, 3, 4, 5, 6 }, { 14, 15, 16, 17, 18, 7 }, { 13, 12, 11, 10, 9, 8 } };
 
 //		int[][] twoDimenMatrix = { { 1, 2, 3, 4, 5, 6 }};
+
+//		int[][] twoDimenMatrix = { { 1 }, { 2 }, { 3 }, { 4 }, { 5 }, { 6 } };
+
+//		int[][] twoDimenMatrix = { { 1, 2, 3 }, { 8, 9, 4 }, { 7, 6, 5 } };
 
 		int left = 0;
 		int right = twoDimenMatrix[0].length - 1;
@@ -42,7 +49,7 @@ public class MxNmattrix {
 			}
 
 			// right -> left (bottom side 1st row)
-			if (top < bottom) {
+			if (top <= bottom) {
 				right--;
 				for (int i = right; i >= left; i--) {
 					System.out.print(twoDimenMatrix[bottom][i] + " ");
@@ -50,9 +57,11 @@ public class MxNmattrix {
 			}
 
 			// bottom -> top (right side 1st column)
-			bottom--;
-			for (int i = bottom; i >= top; i--) {
-				System.out.print(twoDimenMatrix[i][left] + " ");
+			if (right > left) {
+				bottom--;
+				for (int i = bottom; i >= top; i--) {
+					System.out.print(twoDimenMatrix[i][left] + " ");
+				}
 			}
 
 			// moving one step towards inner spiral
